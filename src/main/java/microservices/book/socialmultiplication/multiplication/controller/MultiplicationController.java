6a7acb1a545c2f8e5d3510b0855e1adc99b4af97
Angel Gruevski <angelgruevski@gmail.com>
2018-09-18 12:21:46 +0200

    final

--- src/main/java/microservices/book/socialmultiplication/multiplication/controller/MultiplicationController.java
+++ src/main/java/microservices/book/socialmultiplication/multiplication/controller/MultiplicationController.java
@@ -1,25 +1,31 @@
 package microservices.book.socialmultiplication.multiplication.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import microservices.book.socialmultiplication.multiplication.domain.Multiplication;
 import microservices.book.socialmultiplication.multiplication.service.MultiplicationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping(value = "/multiplications")
 public class MultiplicationController {
 
+    private int serverPort;
     private MultiplicationService multiplicationService;
 
     @Autowired
-    public MultiplicationController(MultiplicationService multiplicationService) {
+    public MultiplicationController(MultiplicationService multiplicationService, @Value("${server.port}") int serverPort) {
         this.multiplicationService = multiplicationService;
+        this.serverPort = serverPort;
     }
 
     @RequestMapping(value = "/random", method = RequestMethod.GET)
     public Multiplication getMultiplication() {
+        log.info("Generating random multiplication from server @{}", serverPort);
         return multiplicationService.createRandomMultiplication();
     }
 }
